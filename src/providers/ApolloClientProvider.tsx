7892540badbfdbb99e796e13bdd8d9e30f9ca9ee
ixedasan <pandikgenshin@gmail.com>
2025-01-13 00:21:05 +0200

    feat(graphql): add codegen and apollo client configuration

---
+++ src/providers/ApolloClientProvider.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { ApolloProvider } from '@apollo/client'
+import type { PropsWithChildren } from 'react'
+import { client } from '@/lib/apollo-client'
+
+export default function ApolloClientProvider({
+	children
+}: PropsWithChildren<unknown>) {
+	return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
