19a50ede07d1459ed9862e303a531e90133b12fb
ixedasan <pandikgenshin@gmail.com>
2025-01-20 00:13:12 +0200

    refactor(browser icons): change icons to static svg

--- src/utils/get-browser-icon.ts
+++ src/utils/get-browser-icon.ts
@@ -1,26 +1,25 @@
 import { CircleHelp } from 'lucide-react'
-import {
-	FaChrome,
-	FaEdge,
-	FaFirefoxBrowser,
-	FaOpera,
-	FaSafari
-} from 'react-icons/fa'
+
+import { ChromeIcon } from '@/components/ui/icons/ChromeIcon'
+import { EdgeIcon } from '@/components/ui/icons/EdgeIcon'
+import { FirefoxIcon } from '@/components/ui/icons/FirefoxIcon'
+import { OperaIcon } from '@/components/ui/icons/OperaIcon'
+import { SafariIcon } from '@/components/ui/icons/SafariIcon'
 
 export function getBrowserIcon(browser: string) {
 	switch (browser.toLowerCase()) {
 		case 'chrome':
-			return FaChrome
+			return ChromeIcon
 		case 'firefox':
-			return FaFirefoxBrowser
+			return FirefoxIcon
 		case 'safari':
-			return FaSafari
+			return SafariIcon
 		case 'edge':
-			return FaEdge
+			return EdgeIcon
 		case 'microsoft edge':
-			return FaEdge
+			return EdgeIcon
 		case 'opera':
-			return FaOpera
+			return OperaIcon
 		default:
 			return CircleHelp
 	}
